d07ab890cabb5332fd9c2ab06713245cdd555faa
vitoleonardo <viktor.krawutschke@gmail.com>
2022-04-25 17:10:38 +0200

    Added documentation and moved merging method into validation class

--- validator/MeshValidator.ts
+++ validator/MeshValidator.ts
@@ -5,7 +5,12 @@ import { Value } from "../models/Value";
 
 /* It takes an event object, checks if the ids are unique, and returns a mesh object */
 export class InputValidator {
-	static validateInput(event): Mesh {
+	/**
+	 * It takes an event object, checks if the ids are unique, checks if the number of elements and values
+	 * are the same, and then merges the values with the elements.
+	 * @returns An array of elements.
+	 */
+	static validateInput(event): Element[] {
 		try {
 			const elements = event.elements as Element[];
 			const values = event.values as Value[];
@@ -21,12 +26,16 @@ export class InputValidator {
 
 			const mesh: Mesh = { elements: elements, values: values, nodes: nodes };
 
-			return mesh;
+			return this.mergeValuesWithElements(mesh);
 		} catch (e) {
 			throw new Error("Failed to parse mesh. Invalid input: " + e);
 		}
 	}
 
+	/**
+	 * It checks if the given array of numbers contains unique values
+	 * @param {number[]} ids - number[] - an array of ids that will be used to create the objects.
+	 */
 	private static checkIfIdUnique(ids: number[]): void {
 		let length = ids.length;
 		let set = new Set(ids);
@@ -34,5 +43,28 @@ export class InputValidator {
 		if (length !== set.size) {
 			throw new Error("Ids have to be unique. Aborting.");
 		}
+	}
+
+	/**
+	 * It takes a mesh and returns an array of elements with values
+	 * @param {Mesh} mesh - Mesh - the mesh object that contains the elements and values
+	 * @returns An array of elements with the value property added.
+	 */
+	private static mergeValuesWithElements(mesh: Mesh): Element[] {
+		let elements: Element[] = [];
+
+		mesh.elements.map((a) => {
+			const value = mesh.values.find((b) => b.element_id === a.id);
+			if (value) {
+				const element: Element = {
+					id: a.id,
+					nodes: a.nodes,
+					value: value.value,
+				};
+				elements.push(element);
+			}
+		});
+
+		return elements;
 	}
 }
