a679cb756aee490c9ef1c9d1d5c4f3186ccebfb2
vitoleonardo <viktor.krawutschke@gmail.com>
2022-04-25 17:18:18 +0200

    removed logs since the task says no logs in stdout

--- handler.ts
+++ handler.ts
@@ -7,18 +7,13 @@ import { InputValidator } from "./validator/MeshValidator";
 /* The function that will be called when the lambda is invoked. It takes a mesh object as argument and returns the n 
 highest viewspots. */
 module.exports.findViewSpot = async (event) => {
-	const AMOUNT_SPOTS_TO_FIND: number = 5;
+	const AMOUNT_SPOTS_TO_FIND: number = Infinity;
 
 	try {
-		const start = new Date().getTime();
-
 		const elements: Element[] = InputValidator.validateInput(event);
 
 		const viewSpotFinder: ViewSpotFinder = new ViewSpotFinder();
-		const spots: Element[] = viewSpotFinder.findSpots(elements);
-
-		const end = new Date().getTime() - start;
-		console.log("Elapsed time in ms: " + end);
+		const spots: Element[] = viewSpotFinder.findSpots(elements, AMOUNT_SPOTS_TO_FIND);
 
 		return { spots: spots };
 	} catch (e) {
